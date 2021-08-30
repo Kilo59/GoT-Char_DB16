1d21b3d680acff9b3a0d907b9a72218e5eec450a
Gabriel <gabriel59kg@gmail.com>
2021-08-29 21:46:00 -0400

    CI steps

--- tasks.py
+++ tasks.py
@@ -3,12 +3,32 @@ import invoke
 
 
 @invoke.task
-def sort(ctx, path="."):
+def sort(ctx, path=".", check=False):
     print("  sorting imports ...")
-    ctx.run(f"isort {path}")
+    args = ["isort", path]
+    if check:
+        args.append("--check")
+    ctx.run(" ".join(args))
 
 
 @invoke.task(pre=[sort])
-def fmt(ctx, path="."):
+def fmt(ctx, path=".", check=False):
     print("  formatting ...")
-    ctx.run(f"black {path}")
+    args = ["black", path]
+    if check:
+        args.append("--check")
+    ctx.run(" ".join(args))
+
+
+@invoke.task
+def check_style(ctx):
+    print("  checking style ...")
+    for cmd in ["sort", "fmt"]:
+        ctx.run(f"invoke {cmd} --check")
+
+
+@invoke.task
+def lint(ctx, path="."):
+    print("  linting ...")
+    print("linting noop")
+    # ctx.run(f"prospector {path}")
