274993a3037f499bb0c3bd69a364eb9e4d5ad8f9
Gabriel <gabriel59kg@gmail.com>
2021-09-04 12:13:21 -0400

    delete sqlite database file before & after test session

--- tests/conftest.py
+++ tests/conftest.py
@@ -3,7 +3,13 @@ tests.conftest.py
 ~~~~~~~~~~~~~~~~~
 Shared testing fixtures
 """
+import pathlib
+
 import pytest
+
+ROOT = pathlib.Path(__file__).parents[1]
+# TODO: don't rely on specific database path or name
+SQLITE_DB = ROOT / "database.db"
 
 
 @pytest.fixture(scope="session")
@@ -11,3 +17,15 @@ def monkey_session():
     """Session scoped monkeypatch"""
     with pytest.MonkeyPatch.context() as monkeypatch:
         yield monkeypatch
+
+
+@pytest.fixture(scope="session", autouse=True)
+def session_setup_teardown():
+    # setup
+    if SQLITE_DB.exists():
+        SQLITE_DB.unlink()  # missing_ok py 3.8
+    # yield
+    yield
+    # teardown
+    if SQLITE_DB.exists():
+        SQLITE_DB.unlink()
